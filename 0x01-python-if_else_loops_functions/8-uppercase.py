fd0ad72813e1319c8793797f289f86ba9632b260
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-06 04:35:28 -0700

    another task done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord(c) in range(97, 123):
+            print(chr(ord(c) - 32), end="")
+        else:
+            print(c, end="")
+    print()
