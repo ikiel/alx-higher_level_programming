be438ebc996bb1c3cf299070076955ad24a46129
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-06 04:56:35 -0700

    another task done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) in range(ord('a'), ord('z')):
+        if ord(c) in range(97, 123):
             s = chr(ord(c) - 32)
         else:
             s = c
