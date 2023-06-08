26c154fb9292c54aaad78e277b12192d8b4a238f
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-08 12:43:08 -0700

    automated commit in 0x02 - python import and modules

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import variable_load_5 as vl
-    print(vl.a)
+    import variable_load_5
+    n = dir(variable_load_5)
+    for i in range (0, len(n)):
+        if n[i] == 'a':
+            print(n[i])
