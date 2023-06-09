93a535c7880e79cb59a222d18964402dbd89a450
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-09 08:59:16 -0700

    automated commit for Python-data structures

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ((x+y) for x in tuple_a for y in tuple_b if len((tuple_a) < 2): x = 0 if len((tuple_b) < 2): y = 0)
+    return new_tuple
