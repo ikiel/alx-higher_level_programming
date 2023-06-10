53e67d09c185091088240cb241a3ba00c797de7b
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-10 02:48:09 -0700

    automated commit for Python-data structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1:
+    if len(matrix) == 1 or matrix is None:
         print()
     else:
         for row in matrix:
