5510242d65a8d4d3f0547a74956e1fe795e799f5
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-12 12:44:08 -0700

    automated commit for Python-data structures

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints basic python list info
+ * @p: PyObject list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, mem, i = 0;
+	PyObject *pyo;
+
+	size = Py_SIZE(p);
+	mem = ((PyListObject *) p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", mem);
+
+	while (i < size)
+	{
+		printf("Element %d: ", i);
+
+		pyo = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(pyo)->tp_name);
+	}
+}
