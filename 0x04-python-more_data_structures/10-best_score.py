fc83c113fa2525ecf0258bc4889c3b0256b74693
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-14 13:22:52 -0700

    corrected indentation error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
- if a_dictionary is None or a_dictionary == {}:
+def best_score(a_dictionary):
+    if a_dictionary is None or a_dictionary == {}:
         return None
-
     max_value = max(a_dictionary, key=lambda k: a_dictionary[k])
     return max_value
