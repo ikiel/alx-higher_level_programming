408e99497014f37067185d538d33d2b0c573818c
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-29 09:31:13 -0700

    task 0: another attempt to fix return value for error cases

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    x = 0
+    j = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
-            x += 1
+            j += 1
             if my_list[i+1] is None:
                 print()
     except IndexError:
         pass
     finally:
-        return (x)
+        return (j)
