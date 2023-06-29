4cec8c1c2cb38d6f67df2a14bdce22aa0c7c9c3b
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-29 09:41:31 -0700

    fixed new line after loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             j += 1
-        print()
     except IndexError:
         pass
     finally:
+        print()
         return (j)
