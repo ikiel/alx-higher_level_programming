25eddca2cf58bf60f4252c809cc8798e883f09a3
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-29 10:52:34 -0700

    removed raise keyword

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             j += 1
     except (TypeError, ValueError):
         pass
-    except IndexError:
-        raise
     else:
         print()
         return j
