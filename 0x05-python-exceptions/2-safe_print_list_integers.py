32ef2d6b71dcb83508aaa57fe5f92617fee814b1
Ezekiel Olojede <jezekiel234@gmail.com>
2023-06-30 03:03:35 -0700

    attempt 1 to fix type error case

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             j += 1
     except (TypeError, ValueError):
         pass
-    finally:
+    else:
         print()
+    finally:
         return j
