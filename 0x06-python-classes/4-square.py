8a7ca63de41f945f64e8b5111f14689225932ec3
Ezekiel Olojede <jezekiel234@gmail.com>
2023-07-02 16:35:14 -0700

    fixed documentation error

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 
-"""define a square class to access and update private attribute"""
+"""define a square class"""
 
 
 class Square:
-    """square class with private size attribute and area"""
+    """
+    square class with a private attribute size
+    """
     def __init__(self, size=0):
         self.size = size
 
-    def area(self):
-        """returns the current square area"""
-        return self.__size ** 2
+    @property
+    def size(self):
+        """retrieve the attribute"""
+        return self.__size
 
     @size.setter
     def size(self, value):
-        """setter method for size"""
-        if type(size) is not int:
+        """modify the attribute value"""
+
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = value
 
-    @property
-    def size(self):
-        """getter method for size"""
-        return self.__size
+    def area(self):
+        """returns the current square area"""
+        return self.__size * self.__size
