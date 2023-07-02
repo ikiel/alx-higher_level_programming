3733cdce566bf1ce24deacfdd7687e8c288e12bf
Ezekiel Olojede <jezekiel234@gmail.com>
2023-07-02 16:08:08 -0700

    tried fixing documentation error

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 
-"""create a square class"""
+"""create a square class to access and update private attribute"""
 
 
 class Square:
     """square class with private size attribute and area"""
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
         self.size = size
 
     def area(self):
