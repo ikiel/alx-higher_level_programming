94832db73480ff130057033c0e3a3cbac8959418
Ezekiel Olojede <jezekiel234@gmail.com>
2023-07-03 05:01:23 -0700

    fixed typo in file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -35,9 +35,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """setter for height"""
-        if type(height) is not int:
+        if type(value) is not int:
             raise TypeError('height must be an integer')
-        if height < 0:
+        if value < 0:
             raise ValueError('height must be >= 0')
 
         self.__height = value
