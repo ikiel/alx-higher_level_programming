7588f58e508f3d903e2690fd101b963727ee9edb
Ezekiel Olojede <jezekiel234@gmail.com>
2023-07-05 08:21:54 -0700

    low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""locked class with no class or object attribute"""
+"""locked class module"""
 
 
 class LockedClass:
-        """Locked class"""
-            __slots__ = ['first_name']
+    """Locked Class"""
+    __slots__ = ["first_name"]
