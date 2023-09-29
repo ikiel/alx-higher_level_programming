0fec023725aed54dbe53b8240bf7fce17eacd45a
Ezekiel Olojede <jezekiel234@gmail.com>
2023-09-29 06:05:48 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/1-write_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-"""This module contains a funcion that writes to a file"""
-
-
-def write_file(filename="", text=""):
-    """This function writes text to a file"""
-    with open(filename, 'w', encoding='UTF8') as file:
-        return file.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-"""This module appends text to a file"""
-
-
-def append_write(filename="", text=""):
-    """This function appends text to a file"""
-    with open(filename, 'a', encoding="UTF8") as a_file:
-        return a_file.write(text)
