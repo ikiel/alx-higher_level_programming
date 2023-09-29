79489bc05f9377529899f5161262ff228015dd5c
Ezekiel Olojede <jezekiel234@gmail.com>
2023-09-29 06:05:20 +0100

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(name=[]):
-    name.append("BestSchool")
-    return ", ".join(name)
--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""locked class module"""
-
-
-class LockedClass:
-    """Locked Class"""
-    __slots__ = ["first_name"]
